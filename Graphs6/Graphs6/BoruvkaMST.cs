3c4a1e2076873b6d3013d67a269ba6c572d39cbf
Vladskor03 <151538733+Vladskor03@users.noreply.github.com>
2023-11-20 20:46:23 +0300

    Add files via upload

---
+++ Graphs6/Graphs6/BoruvkaMST.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graphs6
+{
+    public static class Boruvka
+    {
+        //функция для поиска корня элемента i
+        private static int Find(List<int> parent, int i)
+        {
+            if (parent[i] == i)
+            {
+                return i;
+            }
+            return Find(parent, parent[i]);
+        }
+
+        // Функция, которая выполняет объединение двух остовных деревьев x и y
+        private static void UnionSet(List<int> parent, List<int> rank,
+                              int x, int y)
+        {
+            int xroot = Find(parent, x);
+            int yroot = Find(parent, y);
+
+            // Прикрепляем дерево меньшего ранга к корню дерева высшего ранга
+            if (rank[xroot] < rank[yroot])
+            {
+                parent[xroot] = yroot;
+            }
+            else if (rank[xroot] > rank[yroot])
+            {
+                parent[yroot] = xroot;
+            }
+
+            // Если ранги одинаковы, то делаем один корневой и
+            // увеличим его ранг на единицу
+            else
+            {
+                parent[yroot] = xroot;
+                rank[xroot]++;
+            }
+        }
+
+
+        // TОсновная функция для построения MST 
+       
+        public static List<(int, int, int)> BoruvkaMST(this IGraph graph)
+        {
+            var edges = graph.ListOfEdges();
+            List<int> parent = new List<int>();
+
+            // Массив для хранения индекса самого дешевого ребра подмножества
+            //. В нем хранятся [u,v,w] для каждого компонента
+            List<int> rank = new List<int>();
+            List<List<int>> cheapest = new List<List<int>>();
+            var result = new List<(int, int, int)>();
+            // Изначально существует vertexCount разных деревьев.
+            // Наконец, останется одно дерево, которое будет MST
+            int numTrees = graph.VertexCount();
+
+
+            // Создаeм vertexCount подмножеств с отдельными элементами
+            for (int node = 0; node < graph.VertexCount(); node++)
+            {
+                parent.Add(node);
+                rank.Add(0);
+                cheapest.Add(new List<int> { -1, -1, -1 });
+            }
+
+            // Продолжаeм комбинировать компоненты до тех пор, пока все
+            // компоненты не будут объединены в один MST
+            while (numTrees > 1)
+            {
+
+                // Пройдем по всем ребрам и обновим 
+                // параметры каждого компонента
+                for (int i = 0; i < edges.Count;i++)
+                    
+                {
+
+                    // Найдем компоненты двух углов
+                    // текущего края
+                    int u = edges[i].Item1, v = edges[i].Item2,
+                        w = edges[i].Item3;
+                    int set1 = Find(parent, u),
+                        set2 = Find(parent, v);
+
+                    // Если два угла текущего ребра принадлежат
+                    // одному и тому же набору, игнорируем текущее ребро. Еще раз проверим
+                    // если текущий край ближе к предыдущему
+                    // самые дешевые ребра set1 и set2
+                    if (set1 != set2)
+                    {
+                        if (cheapest[set1][2] == -1
+                            || cheapest[set1][2] > w)
+                        {
+                            cheapest[set1]
+                                = new List<int> { u, v, w };
+                        }
+                        if (cheapest[set2][2] == -1
+                            || cheapest[set2][2] > w)
+                        {
+                            cheapest[set2]
+                                = new List<int> { u, v, w };
+                        }
+                    }
+                }
+
+                // Рассмотрим выбранные выше самые дешевые ребра и
+                // добавим их в MST
+                for (int node = 0; node < graph.VertexCount(); node++)
+                {
+
+                    // Проверим, существует ли самый дешевый вариант для текущего набора
+                    if (cheapest[node][2] != -1)
+                    {
+                        int u = cheapest[node][0],
+                            v = cheapest[node][1],
+                            w = cheapest[node][2];
+                        int set1 = Find(parent, u),
+                            set2 = Find(parent, v);
+                        if (set1 != set2)
+                        {
+                          
+                            UnionSet(parent, rank, set1, set2);
+                            result.Add((u + 1, v + 1, w));
+                            numTrees--;
+                        }
+                    }
+                }
+                for (int node = 0; node < graph.VertexCount(); node++)
+                {
+                    // Обновим cheapest
+                    cheapest[node][2] = -1;
+                }
+            }
+            return result;
+            
+        }
+    }
+}
---
+++ Graphs6/Graphs6/Program.cs
@@ -0,0 +1,143 @@
+﻿using Graphs6;
+using System;
+using System.ComponentModel;
+using System.Diagnostics;
+
+namespace Graphs6 // Note: actual namespace depends on the project name.
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int flag = 0;
+            IGraph? graph = null;
+            StreamWriter? sw = null;
+            int? startVertex = null;
+            int? endVertex = null;
+            for (int i = 0; i < args.Length; i++)
+            {
+                for (int j = 0; j < args.Length; j++)
+                    if (args[j] == "-h")
+                    {
+                        ShowHelp();
+                        flag = 1;
+                        break;
+                    }
+                if (flag == 1)
+                {
+                    break;
+                }
+            }
+            for (int i = 0; i < args.Length; i++)
+            {
+                if (args[i] == "-m" || args[i] == "-e" || args[i] == "-l")
+                {
+                    graph = new GraphMatrix(args[i + 1], args[i]);
+                }
+                else if (args[i] == "-o")
+                {
+                    sw = new StreamWriter(args[i + 1]);
+                    sw.AutoFlush = true;
+                }
+                else if (args[i] == "-n")
+                {
+                    startVertex = int.Parse(args[i + 1]);
+                }
+
+
+            }
+            if (graph == null)
+            {
+                return;
+            }
+            if (sw == null)
+            {
+                sw = new StreamWriter(Console.OpenStandardOutput());
+                sw.AutoFlush = true;
+                Console.SetOut(sw);
+            }
+            flag = 0;
+            foreach (var item in graph.AdjacencyMatrix())
+            {
+                if (item < 0)
+                {
+                    flag = 1;
+                    sw.WriteLine("Graph contains edges with negative weight.");
+                    break;
+                }
+            }
+            if (flag == 0)
+            {
+                sw.WriteLine("Graph does not contain edges with negative weight.");
+            }
+            foreach (var arg in args)
+            {
+                if (arg =="-b")
+                {
+                    var bellmanAlg = graph.BellmanFindShortestPath((int)startVertex);
+                    sw.WriteLine("Bellman-Floyd-Mura: ");
+                    if (bellmanAlg.Item2 == false)
+                    {
+                        int k = 0;
+                        foreach (var item in bellmanAlg.Item1)
+                        {
+                            if (k != (int)startVertex)
+                            {
+                                sw.WriteLine(startVertex + " => " + k + " = " + item);
+
+                            }
+                            k++;
+                        }
+                    }
+                    else
+                    {
+                        sw.WriteLine("the graph does not meet the requirements");
+                    }
+                }
+                if (arg == "-t")
+                {
+                    var levitAlg = graph.LevitFindShortestPath((int)startVertex);
+                    sw.WriteLine("Levit: ");
+                    if (levitAlg.Item2 == false)
+                    {
+                        int k = 0;
+                        foreach (var item in levitAlg.Item1)
+                        {
+                            if (k != (int)startVertex)
+                            {
+                                sw.WriteLine(startVertex + " => " + k + " = " + item);
+                            }
+                            k++;
+                        }
+
+                    }
+                    else
+                    {
+                        sw.WriteLine("the graph does not meet the requirements");
+                    }
+                }
+                if (arg == "-d" )
+                {
+                    if (flag == 0)
+                        if (startVertex != null) graph.Dijkstra((int)startVertex, sw);
+                }
+                    
+            }
+            
+            
+
+            void ShowHelp()
+            {
+                Console.WriteLine("Performed by: Artem Luganskiy\n Group: М3О-225Бк-21 \n Task №6 \n Keys: \n -m - The graph is read from the adjacency matrix \n -e - The graph is read from the list of edges \n -l - The graph is read from the adjacency list \n -n - StartVertex \n -o - The result is output to a file ");
+            }
+            
+        }
+
+
+
+
+
+
+    }
+
+}

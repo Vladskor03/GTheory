936d3556d298b256d251b4184d9ab46786851925
Vladskor03 <151538733+Vladskor03@users.noreply.github.com>
2023-11-20 20:41:57 +0300

    Add files via upload

---
+++ Graphs2/Graphs2/Graph.cs
@@ -0,0 +1,179 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graphs2
+{
+
+    using Vertex = System.Int32;
+    internal class GraphMatrix : IGraph
+    {
+        public int[,] num;
+
+        public GraphMatrix(string path)
+        {
+           
+
+                string[] lines = File.ReadAllLines(path);
+                
+                num = new int[lines.Length, lines.Length];
+            for (int i = 0; i < lines.Length; i++)
+            {
+                string[] temp = lines[i].Split(' ');
+                for (int j = 0; j < temp.Length; j++)
+                    if (temp[j] != "")
+                        num[i, j] = Int32.Parse(temp[j]);
+
+            }
+
+        }//Конструктор Graph
+        public GraphMatrix(int [,] matrix)
+        {
+            num = matrix;
+        }
+        public void GetGraph()
+        {
+            int p = 1;
+
+            foreach (var item in num)
+            {
+                if (p++ % num.GetLength(1) == 0)
+                    Console.WriteLine(item);
+                else
+                    Console.Write(item + "\t");
+            }
+        }//GetGraph
+        public int Weight(Vertex vi, Vertex vj)
+        {
+            if (num[vi, vj] == 0)
+            {
+                Console.WriteLine("There is no such edge");
+            }
+            return num[vi, vj];
+        }//weight
+        public bool IsEdge(Vertex vi, Vertex vj)
+        {
+            if (num[vi, vj] != 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }//is_edge
+        public int[,] AdjacencyMatrix()
+        {
+            
+            int[,] copy = num.Clone() as int[,];
+            return copy;
+        }//adjacency_matrix
+
+        public List<int> AdjacencyList(Vertex v)
+        {
+            List<int> adjacency = new List<int> { };
+            for (Vertex i = 0; i < num.GetUpperBound(0) + 1; i++)
+            {
+                if (i == v)
+                {
+
+                    for (Vertex j = 0; j < num.GetUpperBound(0) + 1; j++)
+                    {
+                        if (num[i, j] != 0)
+                        {
+                            adjacency.Add(j);
+                        }
+                    }
+                    
+                    return adjacency;
+
+
+
+                }
+            }
+            return adjacency;
+        }//adjacency_list
+
+        public List<(int, int, int)> ListOfEdges()
+        {
+            List<(int, int, int)> edges = new List<(int, int, int)>();
+            int k = 1;
+
+            //Console.Write("list_of_edges is: ");
+            for (int i = 0; i < num.GetUpperBound(0) + 1; i++)
+            {
+                for (int j = 0; j < num.GetUpperBound(0) + 1; j++)
+                {
+                    if (num[i, j] != 0)
+                    {
+                        edges.Add((i,j,num[i,j]));
+                    }
+                }
+            }
+            return edges;
+        }//list_of_edges()
+        public List<(int, int, int)> ListOfEdges(Vertex v)
+        {
+            List<(int, int, int)> edges = new List<(int, int, int)>();
+            //Console.Write("list_of_edges Vertex " + v + " is: ");
+            for (int j = 0; j < num.GetUpperBound(0) + 1; j++)
+            {
+                if (num[v, j] != 0)
+                {
+                    edges.Add((v, j, num[v, j]));
+                }
+            }
+            return edges;
+        }//list_of_edges(Vertex v)
+        public bool IsDirected()
+        {
+            for (int i = 0; i < num.GetUpperBound(0) + 1; i++)
+            {
+                for (int j = i + 1; j < num.GetUpperBound(0) + 1; j++)
+                {
+                    if (num[i, j] != num[j, i])
+                    {
+                        return true;
+                    }
+                }
+
+            }
+            return false;
+        }//is_directed()
+
+        
+
+
+
+        //}//VectorOfDegrees()
+        /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        public int VertexCount()
+        {
+            return num.GetUpperBound(0) + 1;
+        }
+        public static void Array_output(int []array)
+        {
+            foreach (var item in array)
+            {
+                Console.Write(item + "    ");
+            }
+            
+        }//Array_output(int []array)
+        public static void Matrix_output(int [,]matrix)
+        {
+            int p = 1;
+
+            foreach (var item in matrix)
+            {
+                if (p++ % matrix.GetLength(1) == 0)
+                    Console.WriteLine(item );
+                else
+                    Console.Write(item + "    ");
+            }
+        }///Matrix_output
+
+    }
+}
---
+++ Graphs2/Graphs2/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.ComponentModel;
+
+
+namespace Graphs2 // Note: actual namespace depends on the project name.
+{
+    public class Program
+    {
+        static async Task Main(string[] args)
+        {
+            int flag = 0;
+            IGraph? graph = null;
+            StreamWriter? sw = null;
+
+            for (int i = 0; i < args.Length; i++)
+            {
+                for (int j = 0; j < args.Length; j++)
+                    if (args[j] == "-h")
+                    {
+                        ShowHelp();
+                        flag = 1;
+                        break;
+                    }
+                if (flag == 1)
+                {
+                    break;
+                }
+            }
+            for (int i = 0; i < args.Length; i++)
+            {
+                if (args[i] == "-m")
+                {
+                    graph = new GraphMatrix(args[i + 1]);
+
+
+                }
+                else if (args[i] == "-e")
+                {
+                    graph = new EdgeList(args[i + 1]);
+
+                }
+                else if (args[i] == "-l")
+                {
+                    graph = new AdjacencyGraph(args[i + 1]);
+                }
+                else if (args[i] == "-o")
+                {
+                    sw = new StreamWriter(args[i + 1]);
+                    sw.AutoFlush = true;
+                }
+
+            }
+            if (graph == null)
+            {
+                return;
+            }
+            if (sw == null)
+            {
+                sw = new StreamWriter(Console.OpenStandardOutput());
+                sw.AutoFlush = true;
+                Console.SetOut(sw);
+            }
+            var connectComponent = graph.IsConnectNotOriented();
+            if (connectComponent.Count == 1)
+                sw.WriteLine("graph is connected.");
+            else sw.WriteLine($"Graph is not connected and contains {connectComponent.Count()} connected components.");
+            sw.WriteLine("Connected components:");
+            for (int i = 0; i < connectComponent.Count(); i++)
+            {
+                foreach (var next in connectComponent[i])
+                {
+                    sw.Write(next + " ");
+                }
+                sw.WriteLine();
+            }
+            if (graph.IsDirected()==true)
+            {
+                
+                var strongly = graph.Kosaraju();
+                if (strongly.Count() == 1)
+                    sw.WriteLine("Digraph is strongly connected.");
+                else sw.WriteLine($"Digraph is weakly connected and contains {strongly.Count()} strongly connected components.");
+
+
+                sw.WriteLine("Strongly connected components:");
+                for (int i = 0; i < strongly.Count(); i++)
+                {
+                    foreach (var next in strongly[i])
+                    {
+                        sw.Write(next + " ");
+                    }
+                    sw.WriteLine();
+                }
+            }
+            
+            void ShowHelp()
+            {
+                Console.WriteLine("Performed by: Artem Luganskiy\n Group: М3О-225Бк-21 \n Task №4 \n Keys: \n -m - The graph is read from the adjacency matrix \n -e - The graph is read from the list of edges \n -l - The graph is read from the adjacency list \n -o - The result is output to a file \n ");
+            }
+        }
+    }
+
+}

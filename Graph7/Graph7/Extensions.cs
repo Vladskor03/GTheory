b9527674bf63e31c1613a849d0eea7f199138a56
Vladskor03 <151538733+Vladskor03@users.noreply.github.com>
2023-11-20 20:47:31 +0300

    Add files via upload

---
+++ Graph7/Graph7/Extensions.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graph7
+{
+    public static class Extensions
+    {
+        public static int[,] Reverse(this IGraph graph)
+        {
+            var graphMatrix = graph.AdjacencyMatrix();
+            for (int i = 0; i < graph.VertexCount(); i++)
+            {
+                for (int j = 0; j < graph.VertexCount(); j++)
+                {
+                    var current = graphMatrix[i, j];
+                    graphMatrix[i, j] = graphMatrix[j, i];
+                    graphMatrix[j, i] = current;
+                }
+            }
+            return graphMatrix;
+        }
+        public static void ArrayOutput(this List<int> array, StreamWriter sw)
+        {
+            foreach (var item in array)
+            {
+                sw.Write(item + "  ");
+            }
+
+        }//Array_output(int []array)
+        public static void Matrix_output(int[,] matrix, StreamWriter sw)
+        {
+            int p = 1;
+
+            foreach (var item in matrix)
+            {
+                if (p++ % matrix.GetLength(1) == 0)
+                    if (item == int.MaxValue)
+                    {
+                        sw.WriteLine("∞");
+                    }
+                    else sw.WriteLine(item);
+                else
+                    if (item == int.MaxValue)
+                {
+                    sw.Write("∞" + " ");
+                }
+                else sw.Write(item + " ");
+            }
+        }///Matrix_output
+    }
+}
---
+++ Graph7/Graph7/IGraph.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graph7
+{
+    using Vertex = System.Int32;
+    public interface IGraph
+    {
+        public int Weight(Vertex vi, Vertex vj);
+        public bool IsEdge(Vertex vi, Vertex vj);
+        public int[,] AdjacencyMatrix();
+        public List<int> AdjacencyList(Vertex v);
+        public List<(int, int, int)> ListOfEdges();
+        public List<(int, int, int)> ListOfEdges(Vertex v);
+        public bool IsDirected();
+        public int VertexCount();
+    }
+}
